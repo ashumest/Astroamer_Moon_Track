3a13cb536477d0a845b88e91d3fdef6fb4456a7c
ashumest <ashumest@gmail.com>
2018-04-11 20:48:34 +0530

    Update moon.js

--- scripts/moon.js
+++ scripts/moon.js
@@ -32,7 +32,6 @@ var attempt = {
 }
 var moon = {
     start: function (lng) {
-        setuserdetail();
         myData.language = lng;
         myData.userId = $.cookie("user_id");
         myData.buddyIds = $.cookie("buddy_ids");
